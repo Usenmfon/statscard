6f06a76a8b0b7df8e421c55e5c9ea282a0a7e151
Usenmfon <usenmfonuko@gmail.com>
2021-07-16 16:06:03 +0100

    centred div

---
+++ script.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+    const heading =  document.createElement("h1");
+    const heading_text = document.createTextNode("Big Head");
+    heading.appendChild(heading_text);
+    document.body.appendChild(heading);
+}
+
+window.onload = alert("Hello There")
+
+
+window.onload = function(){
+    const heading = document.createElement('h2');
+    const heading_text = document.createTextNode("Hello Small Head");
+    heading.appendChild(heading_text);
+    document.body.appendChild(heading);
+}
